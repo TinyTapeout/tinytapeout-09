ce786e056d4c25fc857edcad314d30bf942a7cae
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2024-10-09 11:53:15 +0300

    feat: update project tt_um_brandonramos_opamp_ladder from J0NTrollston/tt08-analog-adc
    Commit: 01a1d07a1a1caf4ad7cd5710827a48989553858e
    Workflow: https://github.com/J0NTrollston/tt08-analog-adc/actions/runs/11251407136

---
+++ projects/tt_um_brandonramos_opamp_ladder/docs/info.md
@@ -0,0 +1,17 @@
+<!---
+
+This file is used to generate your project datasheet. Please fill in the information below and delete any unused
+sections.
+
+You can also include images in this folder and reference them in the markdown. Each image must be less than
+512 kb in size, and the combined size of all images must be less than 1 MB.
+-->
+
+## How it works
+Converts a 1.8v to 0v analog signal into an encoded 2-bit digital signal
+
+## How to test
+You can test by taking a voltage of 0 to 1.8v to the analog pin 0 and the encoded bits come out to the dedicated outputs 0 and 1
+
+## External hardware
+N/A
---
+++ projects/tt_um_brandonramos_opamp_ladder/tt_um_brandonramos_opamp_ladder.v
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_brandonramos_opamp_ladder (
+    input  wire       VGND,
+    input  wire       VDPWR,    // 1.8v power supply
+//    input  wire       VAPWR,    // 3.3v power supply
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+wire [3:0] analog_to_digital_in;
+
+//digital
+adc_digital_control adc_digital_control(
+    .analog_to_digital_in(analog_to_digital_in),
+    .encoded_out(uo_out[1:0]),
+    .VPWR(VDPWR),
+    .VGND(VGND)
+    );
+
+//analog
+opamp_ladder opamp_ladder(
+    .in(ua[0]),
+    .out0(analog_to_digital_in[0]),
+    .out1(analog_to_digital_in[1]),
+    .out2(analog_to_digital_in[2]),
+    .out3(analog_to_digital_in[3]),
+    .VDD(VDPWR),
+    .VSS(VGND)
+    );
+
+
+    // ties for the output enables
+    // assign uo_out[0] = VGND;
+    // assign uo_out[1] = VGND;
+    assign uo_out[2] = VGND;
+    assign uo_out[3] = VGND;
+    assign uo_out[4] = VGND;
+    assign uo_out[5] = VGND;
+    assign uo_out[6] = VGND;
+    assign uo_out[7] = VGND;
+
+    assign uio_out[0] = VGND;
+    assign uio_out[1] = VGND;
+    assign uio_out[2] = VGND;
+    assign uio_out[3] = VGND;
+    assign uio_out[4] = VGND;
+    assign uio_out[5] = VGND;
+    assign uio_out[6] = VGND;
+    assign uio_out[7] = VGND;
+
+    assign uio_oe[0] = VGND;
+    assign uio_oe[1] = VGND;
+    assign uio_oe[2] = VGND;
+    assign uio_oe[3] = VGND;
+    assign uio_oe[4] = VGND;
+    assign uio_oe[5] = VGND;
+    assign uio_oe[6] = VGND;
+    assign uio_oe[7] = VGND;
+
+endmodule
